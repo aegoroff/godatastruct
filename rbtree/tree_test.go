fe4ded020b78d2f590ae1e184a882e4ec2994d19
egoroff <egoroff@gmail.com>
2018-10-11 14:29:14 +0300

    Revert fix

--- rbtree/tree_test.go
+++ rbtree/tree_test.go
@@ -3,7 +3,6 @@ package rbtree
 import (
 	"fmt"
 	"github.com/stretchr/testify/assert"
-	"gonum.org/v1/gonum/graph"
 	"gonum.org/v1/gonum/graph/encoding"
 	"gonum.org/v1/gonum/graph/encoding/dot"
 	"gonum.org/v1/gonum/graph/simple"
@@ -381,9 +380,7 @@ func getTreeAsGraphviz(tree *RbTree) string {
 		gn := &GraphNode{Node: node, NodeID: id}
 		gr.AddNode(gn)
 		id++
-		var nodes []graph.Node
-		nodes = append(nodes, gr.Nodes()...)
-		for _, n := range nodes {
+		for _, n := range gr.Nodes() {
 			if node.Parent.Key != nil && n.(*GraphNode).Node.Key == node.Parent.Key {
 				edge := gr.NewEdge(n, gn)
 				gr.SetEdge(edge)
