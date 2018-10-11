ab7fa95320bd69ee56840f56959298bb98bff641
egoroff <egoroff@gmail.com>
2018-10-11 13:11:16 +0300

    Prepare to make public build

--- rbtree/search.go
+++ rbtree/search.go
@@ -1,90 +1,90 @@
 // This file contains all RB tree search methods implementations
 package rbtree
 
-// Searches value specified within search tree
+// Search searches value specified within search tree
 func Search(root *Node, value *Comparable) (*Node, bool) {
-    var x *Node
-    x = root
-    for x != nil && x.Key != nil && !(*value).EqualTo(*x.Key) {
-        if (*value).LessThan(*x.Key) {
-            x = x.Left
-        } else {
-            x = x.Right
-        }
-    }
-    ok := x != nil && x.Key != nil
+	var x *Node
+	x = root
+	for x != nil && x.Key != nil && !(*value).EqualTo(*x.Key) {
+		if (*value).LessThan(*x.Key) {
+			x = x.Left
+		} else {
+			x = x.Right
+		}
+	}
+	ok := x != nil && x.Key != nil
 
-    if !ok {
-        return nil, ok
-    }
+	if !ok {
+		return nil, ok
+	}
 
-    return x, ok
+	return x, ok
 }
 
-// Gets tree's min element
+// Minimum gets tree's min element
 func Minimum(root *Node) *Node {
-    x := root
-    for x.Left != nil && x.Left.Key != nil {
-        x = x.Left
-    }
-    return x
+	x := root
+	for x.Left != nil && x.Left.Key != nil {
+		x = x.Left
+	}
+	return x
 }
 
-// Gets tree's max element
+// Maximum gets tree's max element
 func Maximum(root *Node) *Node {
-    x := root
-    for x.Right != nil && x.Right.Key != nil {
-        x = x.Right
-    }
-    return x
+	x := root
+	for x.Right != nil && x.Right.Key != nil {
+		x = x.Right
+	}
+	return x
 }
 
-// Gets node specified successor
+// Successor gets node specified successor
 func Successor(n *Node) *Node {
-    if n.Right != nil && n.Right.Key != nil {
-        return Minimum(n.Right)
-    }
+	if n.Right != nil && n.Right.Key != nil {
+		return Minimum(n.Right)
+	}
 
-    y := n.Parent
-    for y != nil && y.Key != nil && n == y.Right {
-        n = y
-        y = y.Parent
-    }
+	y := n.Parent
+	for y != nil && y.Key != nil && n == y.Right {
+		n = y
+		y = y.Parent
+	}
 
-    if y.Key == nil {
-        return nil
-    }
+	if y.Key == nil {
+		return nil
+	}
 
-    return y
+	return y
 }
 
 // Gets node specified predecessor
 func Predecessor(n *Node) *Node {
-    if n.Left != nil && n.Left.Key != nil {
-        return Maximum(n.Left)
-    }
+	if n.Left != nil && n.Left.Key != nil {
+		return Maximum(n.Left)
+	}
 
-    y := n.Parent
-    for y != nil && y.Key != nil && n == y.Left {
-        n = y
-        y = y.Parent
-    }
+	y := n.Parent
+	for y != nil && y.Key != nil && n == y.Left {
+		n = y
+		y = y.Parent
+	}
 
-    if y.Key == nil {
-        return nil
-    }
+	if y.Key == nil {
+		return nil
+	}
 
-    return y
+	return y
 }
 
 // Gets i element from subtree
 func OrderStatisticSelect(root *Node, i int64) *Node {
-    r := root.Left.Size + 1
-    if i == r {
-        return root
-    } else if i < r {
-        return OrderStatisticSelect(root.Left, i)
-    } else {
-        return OrderStatisticSelect(root.Right, i-r)
-    }
+	r := root.Left.Size + 1
+	if i == r {
+		return root
+	} else if i < r {
+		return OrderStatisticSelect(root.Left, i)
+	} else {
+		return OrderStatisticSelect(root.Right, i-r)
+	}
 }
--- rbtree/tree.go
+++ rbtree/tree.go
@@ -2,58 +2,62 @@
 package rbtree
 
 const (
-    Black = iota
-    Red
+	// Black RB tree node
+	Black = iota
+
+	// Red RB tree node
+	Red
 )
 
 type RbTree struct {
-    Root *Node
-    tnil *Node
+	Root *Node
+	tnil *Node
 }
 
-// Represent red-black tree node
+// Node represent red-black tree node
 type Node struct {
-    // Node key (data)
-    Key    *Comparable
-    Parent *Node
-    Left   *Node
-    Right  *Node
-    // Node color (red or black)
-    Color int
-    // Subtree size including node itself
-    Size int64
+	// Node key (data)
+	Key    *Comparable
+	Parent *Node
+	Left   *Node
+	Right  *Node
+	// Node color (red or black)
+	Color int
+	// Subtree size including node itself
+	Size int64
 }
 
+// Comparable defines comparable type interface
 type Comparable interface {
-    LessThan(y interface{}) bool
-    EqualTo(y interface{}) bool
+	LessThan(y interface{}) bool
+	EqualTo(y interface{}) bool
 }
 
-// Creates new Red-Black empty tree
+// NewRbTree creates new Red-Black empty tree
 func NewRbTree() *RbTree {
-    tnil := Node{Color: Black}
-    return &RbTree{tnil: &tnil}
+	tnil := Node{Color: Black}
+	return &RbTree{tnil: &tnil}
 }
 
-// Creates new node
+// NewNode creates new node
 func NewNode(si Comparable) *Node {
-    return &Node{Key: &si}
+	return &Node{Key: &si}
 }
 
-// Walks tree inorder (left, node, right)
+// WalkInorder walks tree inorder (left, node, right)
 func WalkInorder(root *Node, action func(*Node)) {
-    if root != nil && root.Key != nil {
-        WalkInorder(root.Left, action)
-        action(root)
-        WalkInorder(root.Right, action)
-    }
+	if root != nil && root.Key != nil {
+		WalkInorder(root.Left, action)
+		action(root)
+		WalkInorder(root.Right, action)
+	}
 }
 
-// Walks tree preorder (node, left, right)
+// WalkPreorder walks tree preorder (node, left, right)
 func WalkPreorder(root *Node, action func(*Node)) {
-    if root != nil && root.Key != nil {
-        action(root)
-        WalkPreorder(root.Left, action)
-        WalkPreorder(root.Right, action)
-    }
+	if root != nil && root.Key != nil {
+		action(root)
+		WalkPreorder(root.Left, action)
+		WalkPreorder(root.Right, action)
+	}
 }
