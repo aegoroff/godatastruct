2ece5d933c4df0aa5cc7fe929ccb167600fadf73
egoroff <egoroff@gmail.com>
2018-10-13 11:33:57 +0300

    Package description added

--- countingsort/countingsort.go
+++ countingsort/countingsort.go
@@ -1,3 +1,4 @@
+// Package countingsort contains counting sort methods that sorts int or in64 slice using counting sort algorithm
 package countingsort
 
 // Ints sorts integers slice using counting sort algorithm
