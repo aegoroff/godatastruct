af033cb3244cdd0aab61ee55d2546463a868e825
egoroff <egoroff@gmail.com>
2020-05-18 21:48:57 +0300

    collections module added only with one collection - hashset

---
+++ collections/hashset.go
@@ -0,0 +1,57 @@
+package collections
+
+// StringHashSet defines strings hash set
+type StringHashSet map[string]interface{}
+
+// IntHashSet defines integers hash set
+type IntHashSet map[int]interface{}
+
+// Count gets the number of items in the set
+func (s *StringHashSet) Count() int {
+	return len(*s)
+}
+
+// Items gets all set's items
+func (s *StringHashSet) Items() []string {
+	keys := make([]string, 0, len(*s))
+	for k := range *s {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+// Contains gets whether a key is presented within the set
+func (s *StringHashSet) Contains(key string) bool {
+	_, ok := (*s)[key]
+	return ok
+}
+
+// Add adds new item into the set
+func (s *StringHashSet) Add(key string) {
+	(*s)[key] = nil
+}
+
+// Count gets the number of items in the set
+func (s *IntHashSet) Count() int {
+	return len(*s)
+}
+
+// Items gets all set's items
+func (s *IntHashSet) Items() []int {
+	keys := make([]int, 0, len(*s))
+	for k := range *s {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+// Contains gets whether a key is presented within the set
+func (s *IntHashSet) Contains(key int) bool {
+	_, ok := (*s)[key]
+	return ok
+}
+
+// Add adds new item into the set
+func (s *IntHashSet) Add(key int) {
+	(*s)[key] = nil
+}
---
+++ collections/hashset_test.go
@@ -0,0 +1,219 @@
+package collections
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+// IntHashSet
+
+func TestIntHashSet_EmptySet_NoKey(t *testing.T) {
+	// Arrange
+	ass := assert.New(t)
+	var set = make(IntHashSet)
+
+	// Act
+
+	// Assert
+	_, ok := set[1]
+	ass.False(ok)
+}
+
+func TestIntHashSet_AddItem_ItemAdded(t *testing.T) {
+	// Arrange
+	ass := assert.New(t)
+	var set = make(IntHashSet)
+
+	// Act
+	set.Add(1)
+
+	// Assert
+	_, ok := set[1]
+	ass.True(ok)
+}
+
+func TestIntHashSet_Contains_ResultTrue(t *testing.T) {
+	// Arrange
+	ass := assert.New(t)
+	var set = make(IntHashSet)
+	set.Add(1)
+
+	// Act
+	ok := set.Contains(1)
+
+	// Assert
+	ass.True(ok)
+}
+
+func TestIntHashSet_ContainsUnexistKey_ResultFalse(t *testing.T) {
+	// Arrange
+	ass := assert.New(t)
+	var set = make(IntHashSet)
+	set.Add(1)
+
+	// Act
+	ok := set.Contains(2)
+
+	// Assert
+	ass.False(ok)
+}
+
+func TestIntHashSet_Count_ResultAsSpecified(t *testing.T) {
+	// Arrange
+	ass := assert.New(t)
+
+	var tests = []struct {
+		items []int
+		count int
+	}{
+		{[]int{1}, 1},
+		{[]int{}, 0},
+		{[]int{1, 2}, 2},
+	}
+
+	for _, tt := range tests {
+		var set = make(IntHashSet)
+		for _, i := range tt.items {
+			set.Add(i)
+		}
+
+		// Act
+		count := set.Count()
+
+		// Assert
+		ass.Equal(tt.count, count)
+	}
+}
+
+func TestIntHashSet_Items_ResultAsSpecified(t *testing.T) {
+	// Arrange
+	ass := assert.New(t)
+
+	var tests = []struct {
+		items []int
+	}{
+		{[]int{1}},
+		{[]int{}},
+		{[]int{3, 1}},
+	}
+
+	for _, tt := range tests {
+		var set = make(IntHashSet)
+		for _, i := range tt.items {
+			set.Add(i)
+		}
+
+		// Act
+		items := set.Items()
+
+		// Assert
+		ass.ElementsMatch(tt.items, items)
+	}
+}
+
+// StringHashSet
+
+func TestStringHashSet_EmptySet_NoKey(t *testing.T) {
+	// Arrange
+	ass := assert.New(t)
+	var set = make(StringHashSet)
+
+	// Act
+
+	// Assert
+	_, ok := set["1"]
+	ass.False(ok)
+}
+
+func TestStringHashSet_AddItem_ItemAdded(t *testing.T) {
+	// Arrange
+	ass := assert.New(t)
+	var set = make(StringHashSet)
+
+	// Act
+	set.Add("1")
+
+	// Assert
+	_, ok := set["1"]
+	ass.True(ok)
+}
+
+func TestStringHashSet_Contains_ResultTrue(t *testing.T) {
+	// Arrange
+	ass := assert.New(t)
+	var set = make(StringHashSet)
+	set.Add("1")
+
+	// Act
+	ok := set.Contains("1")
+
+	// Assert
+	ass.True(ok)
+}
+
+func TestStringHashSet_ContainsUnexistKey_ResultFalse(t *testing.T) {
+	// Arrange
+	ass := assert.New(t)
+	var set = make(StringHashSet)
+	set.Add("1")
+
+	// Act
+	ok := set.Contains("2")
+
+	// Assert
+	ass.False(ok)
+}
+
+func TestStringHashSet_Count_ResultAsSpecified(t *testing.T) {
+	// Arrange
+	ass := assert.New(t)
+
+	var tests = []struct {
+		items []string
+		count int
+	}{
+		{[]string{"1"}, 1},
+		{[]string{}, 0},
+		{[]string{"1", "2"}, 2},
+	}
+
+	for _, tt := range tests {
+		var set = make(StringHashSet)
+		for _, i := range tt.items {
+			set.Add(i)
+		}
+
+		// Act
+		count := set.Count()
+
+		// Assert
+		ass.Equal(tt.count, count)
+	}
+}
+
+func TestStringHashSet_Items_ResultAsSpecified(t *testing.T) {
+	// Arrange
+	ass := assert.New(t)
+
+	var tests = []struct {
+		items []string
+	}{
+		{[]string{"1"}},
+		{[]string{}},
+		{[]string{"3", "1"}},
+	}
+
+	for _, tt := range tests {
+		var set = make(StringHashSet)
+		for _, i := range tt.items {
+			set.Add(i)
+		}
+
+		// Act
+		items := set.Items()
+
+		// Assert
+		ass.Equal(tt.items, items)
+		ass.ElementsMatch(tt.items, items)
+	}
+}
