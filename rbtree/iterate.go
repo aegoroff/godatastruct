6017f2f372caac80a2b5053e2fb7280d600a8f6d
egoroff <egoroff@gmail.com>
2020-05-31 16:03:20 +0300

    Conditions simplified

--- rbtree/iterate.go
+++ rbtree/iterate.go
@@ -81,7 +81,7 @@ func (tree *rbTree) DescendRange(from, to Comparable, iterator NodeIterator) {
 
 func (n *node) ascend(from, to Comparable, iterator NodeIterator) {
 	curr, ok := n.search(from)
-	for ok && curr != nil && curr.key != nil && (curr).LessThan(to) || curr != nil && curr.key != nil && (curr).EqualTo(to) {
+	for ok && curr != nil && curr.key != nil && (curr.LessThan(to) || curr.EqualTo(to)) {
 		ok = iterator(curr)
 		if ok {
 			curr = curr.successor()
@@ -91,7 +91,7 @@ func (n *node) ascend(from, to Comparable, iterator NodeIterator) {
 
 func (n *node) descend(from, to Comparable, iterator NodeIterator) {
 	curr, ok := n.search(from)
-	for ok && curr != nil && curr.key != nil && (!curr.LessThan(to) || curr.key != nil && curr.EqualTo(to)) {
+	for ok && curr != nil && curr.key != nil && (!curr.LessThan(to) || curr.EqualTo(to)) {
 		ok = iterator(curr)
 		if ok {
 			curr = curr.predecessor()
