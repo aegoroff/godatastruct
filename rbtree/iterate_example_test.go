026d5a35746968bfe55e16f17cadfbdfa242cf74
egoroff <egoroff@gmail.com>
2020-12-06 14:47:36 +0300

    All iterators examples added

---
+++ rbtree/iterate_example_test.go
@@ -0,0 +1,143 @@
+package rbtree
+
+import (
+	"fmt"
+)
+
+func ExampleNewWalkInorder() {
+	tree := NewRbTree()
+
+	tree.Insert(NewInt(6))
+	tree.Insert(NewInt(18))
+	tree.Insert(NewInt(3))
+
+	it := NewWalkInorder(tree)
+
+	it.Iterate(func(n Node) bool {
+		fmt.Println(n.Key())
+		return true
+	})
+
+	// Output
+	// 3
+	// 6
+	// 18
+}
+
+func ExampleNewWalkPreorder() {
+	tree := NewRbTree()
+
+	tree.Insert(NewInt(6))
+	tree.Insert(NewInt(18))
+	tree.Insert(NewInt(3))
+
+	it := NewWalkPreorder(tree)
+
+	it.Iterate(func(n Node) bool {
+		fmt.Println(n.Key())
+		return true
+	})
+
+	// Output
+	// 6
+	// 3
+	// 18
+}
+
+func ExampleNewWalkPostorder() {
+	tree := NewRbTree()
+
+	tree.Insert(NewInt(6))
+	tree.Insert(NewInt(18))
+	tree.Insert(NewInt(3))
+
+	it := NewWalkPostorder(tree)
+
+	it.Iterate(func(n Node) bool {
+		fmt.Println(n.Key())
+		return true
+	})
+
+	// Output
+	// 3
+	// 18
+	// 6
+}
+
+func ExampleNewAscend() {
+	tree := NewRbTree()
+
+	tree.Insert(NewInt(6))
+	tree.Insert(NewInt(18))
+	tree.Insert(NewInt(3))
+
+	it := NewAscend(tree)
+
+	it.Iterate(func(n Node) bool {
+		fmt.Println(n.Key())
+		return true
+	})
+
+	// Output
+	// 3
+	// 6
+	// 18
+}
+
+func ExampleNewDescend() {
+	tree := NewRbTree()
+
+	tree.Insert(NewInt(6))
+	tree.Insert(NewInt(18))
+	tree.Insert(NewInt(3))
+
+	it := NewDescend(tree)
+
+	it.Iterate(func(n Node) bool {
+		fmt.Println(n.Key())
+		return true
+	})
+
+	// Output
+	// 18
+	// 6
+	// 3
+}
+
+func ExampleNewAscendRange() {
+	tree := NewRbTree()
+
+	tree.Insert(NewInt(6))
+	tree.Insert(NewInt(18))
+	tree.Insert(NewInt(3))
+
+	it := NewAscendRange(tree, NewInt(3), NewInt(6))
+
+	it.Iterate(func(n Node) bool {
+		fmt.Println(n.Key())
+		return true
+	})
+
+	// Output
+	// 3
+	// 6
+}
+
+func ExampleNewDescendRange() {
+	tree := NewRbTree()
+
+	tree.Insert(NewInt(6))
+	tree.Insert(NewInt(18))
+	tree.Insert(NewInt(3))
+
+	it := NewDescendRange(tree, NewInt(6), NewInt(3))
+
+	it.Iterate(func(n Node) bool {
+		fmt.Println(n.Key())
+		return true
+	})
+
+	// Output
+	// 6
+	// 3
+}
