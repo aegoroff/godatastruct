25d27d19216c413e394c5911b6cae63729edaba7
egoroff <egoroff@gmail.com>
2020-12-15 11:17:47 +0300

    more iterators tests implemented

--- rbtree/iterate_test.go
+++ rbtree/iterate_test.go
@@ -61,6 +61,68 @@ func Test_Foreach_Normal(t *testing.T) {
 	}
 }
 
+func Test_Foreach_SpecialCases(t *testing.T) {
+	var tests = []struct {
+		name     string
+		itFunc   func(t RbTree) Enumerable
+		input    []int
+		expected []int
+	}{
+		{"ascend all eq three", NewAscend, []int{2, 2, 2}, []int{2, 2, 2}},
+		{"ascend all eq two", NewAscend, []int{2, 2}, []int{2, 2}},
+		{"ascend all not eq two", NewAscend, []int{1, 2}, []int{1, 2}},
+		{"ascend all eq one", NewAscend, []int{2}, []int{2}},
+		{"ascend all eq zero", NewAscend, []int{}, []int{}},
+
+		{"descend all eq three", NewDescend, []int{2, 2, 2}, []int{2, 2, 2}},
+		{"descend all eq two", NewDescend, []int{2, 2}, []int{2, 2}},
+		{"descend all not eq two", NewDescend, []int{1, 2}, []int{2, 1}},
+		{"descend all eq one", NewDescend, []int{2}, []int{2}},
+		{"descend all eq zero", NewDescend, []int{}, []int{}},
+
+		{"inorder all eq three", NewWalkInorder, []int{2, 2, 2}, []int{2, 2, 2}},
+		{"inorder all eq two", NewWalkInorder, []int{2, 2}, []int{2, 2}},
+		{"inorder all not eq two", NewWalkInorder, []int{1, 2}, []int{1, 2}},
+		{"inorder all eq one", NewWalkInorder, []int{2}, []int{2}},
+		{"inorder all eq zero", NewWalkInorder, []int{}, []int{}},
+
+		{"preorder all eq three", NewWalkPreorder, []int{2, 2, 2}, []int{2, 2, 2}},
+		{"preorder all eq two", NewWalkPreorder, []int{2, 2}, []int{2, 2}},
+		{"preorder all not eq two", NewWalkPreorder, []int{1, 2}, []int{1, 2}},
+		{"preorder all eq one", NewWalkPreorder, []int{2}, []int{2}},
+		{"preorder all eq zero", NewWalkPreorder, []int{}, []int{}},
+
+		{"postorder all eq three", NewWalkPostorder, []int{2, 2, 2}, []int{2, 2, 2}},
+		{"postorder all eq two", NewWalkPostorder, []int{2, 2}, []int{2, 2}},
+		{"postorder all not eq two", NewWalkPostorder, []int{1, 2}, []int{2, 1}},
+		{"postorder all eq one", NewWalkPostorder, []int{2}, []int{2}},
+		{"postorder all eq zero", NewWalkPostorder, []int{}, []int{}},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			// Arrange
+			ass := assert.New(t)
+
+			tree := NewRbTree()
+			for _, n := range test.input {
+				tree.Insert(NewInt(n))
+			}
+
+			it := test.itFunc(tree)
+
+			result := make([]int, 0)
+
+			// Act
+			it.Foreach(func(n Node) {
+				result = append(result, GetInt(n.Key()))
+			})
+
+			// Assert
+			ass.Equal(test.expected, result)
+		})
+	}
+}
+
 func Test_IteratorsWithInterruption_Normal(t *testing.T) {
 	tree := createIntegerTestTree()
 	var tests = []struct {
