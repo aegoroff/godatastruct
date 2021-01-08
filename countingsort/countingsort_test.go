8815074b363d10e73907edde331a49f8e3bb6905
egoroff <egoroff@gmail.com>
2021-01-09 01:34:51 +0300

    test: TestInts64Unstable_Random added

--- countingsort/countingsort_test.go
+++ countingsort/countingsort_test.go
@@ -86,6 +86,18 @@ func TestInts64Unstable(t *testing.T) {
 	ass.Equal([]int64{0, 0, 1, 2, 2, 3, 3, 3, 4, 5}, items)
 }
 
+func TestInts64Slice_Sort(t *testing.T) {
+	// Arrange
+	ass := assert.New(t)
+	items := []int64{2, 5, 3, 0, 2, 3, 0, 3, 4, 1}
+
+	// Act
+	sort.Sort(Int64Slice(items))
+
+	// Assert
+	ass.Equal([]int64{0, 0, 1, 2, 2, 3, 3, 3, 4, 5}, items)
+}
+
 func TestInts64Unstable_Random(t *testing.T) {
 	// Arrange
 	ass := assert.New(t)
