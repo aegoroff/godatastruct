58f4d0c86d451186ff63a0e2ce897a6728fc6d9d
egoroff <egoroff@gmail.com>
2022-03-24 00:00:16 +0300

    refactor: Integer interface separated into two smaller interfaces Signed and Unsigned

--- types/ifaces.go
+++ types/ifaces.go
@@ -2,5 +2,15 @@ package types
 
 // Integer defines all integer types interface
 type Integer interface {
-	int | int64 | int16 | int8 | int32 | uint | uint8 | uint16 | uint32 | uint64
+	Signed | Unsigned
+}
+
+// Signed is a constraint that permits any signed integer type.
+type Signed interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64
+}
+
+// Unsigned is a constraint that permits any unsigned integer type.
+type Unsigned interface {
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
 }
